4ce33893158b1e7f97490fdc40886819aec9276a
Shea Martin <shea.martin@gmail.com>
2026-02-04 14:57:58 -0400

    fix up aliases

---
+++ alias.linux.sh
@@ -0,0 +1,8 @@
+function yzd() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+	command yazi "$@" --cwd-file="$tmp"
+	if cwd="$(cat "$tmp")"; then
+		[ -d "$cwd" ] && cd "$cwd"
+	fi
+	rm -f "$tmp"
+}
